9eb77177a6b2ce3525105b398066dd9274a60130
TheSecurity <jamiedesert01@gmail.com>
2022-09-17 08:49:46 +0200

    Add async approach, SQLite storage

---
+++ src/SaveIT.Core/Storage/SQLiteStorage.cs
@@ -0,0 +1,48 @@
+﻿using SQLite;
+using System.Linq.Expressions;
+
+namespace SaveIT.Core.Storage
+{
+    public class SQLiteStorage<T> : IStorage<T> where T : new()
+    {
+        // This doesn't work untill MAUI 8.0, Nov 10
+        //private static string dataDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "SaveIT");
+        private static string dataDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "SaveIT");
+        private const string fileName = "data.db";
+        private static string filePath = Path.Combine(dataDirectory, fileName);
+
+        private readonly SQLiteAsyncConnection _connection;
+
+        public SQLiteStorage()
+        {
+            var res = Directory.CreateDirectory(dataDirectory);
+            _connection = new SQLiteAsyncConnection(filePath, SQLiteOpenFlags.Create | SQLiteOpenFlags.ReadWrite);
+            _ = _connection.CreateTableAsync<T>().Result;
+        }
+
+        public async Task CreateAsync(T item)
+            => await _connection.InsertAsync(item);
+
+        public async Task DeleteAsync(Expression<Func<T, bool>> exp)
+            => await _connection.DeleteAsync(exp);
+
+        public async Task<T?> GetAsync(Expression<Func<T, bool>> exp)
+            => await _connection.Table<T>()
+                .FirstOrDefaultAsync(exp);
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+            => await _connection.Table<T>()
+                .ToListAsync();
+
+        public async Task<IEnumerable<T>> GetManyAsync(Expression<Func<T, bool>> exp)
+            => await _connection.Table<T>()
+                .Where(exp)
+                .ToListAsync();
+
+        public async Task UpdateAsync(Expression<Func<T, bool>> exp, T item)
+            => await _connection.Table<T>()
+                .Where(exp)
+                .ToListAsync();
+
+    }
+}

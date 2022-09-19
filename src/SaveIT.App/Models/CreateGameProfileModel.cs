0c8786390f8a27fca35bf924fa2009bbed37ae21
TheSecurity <jamiedesert01@gmail.com>
2022-09-19 22:56:20 +0200

    reorder attributes

--- src/SaveIT.App/Models/CreateGameProfileModel.cs
+++ src/SaveIT.App/Models/CreateGameProfileModel.cs
@@ -5,12 +5,11 @@ namespace SaveIT.App.Models;
 
 public class CreateGameProfileModel
 {
-	[Required(ErrorMessage = "Profile name has to be set.")]
 	[DisplayName("Profile name")]
-
+	[Required(ErrorMessage = "Profile name has to be set.")]
 	public string ProfileName { get; set; } = null!;
 
-	[Required(ErrorMessage = "Nickname has to be set.")]
 	[DisplayName("Nickname")]
+	[Required(ErrorMessage = "Nickname has to be set.")]
 	public string Nickname { get; set; } = null!;
 }

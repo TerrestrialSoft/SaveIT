846a2d8969f7b588871345d9eb6c33e39c76d310
TheSecurity <jamiedesert01@gmail.com>
2022-10-04 13:18:21 +0200

    add check for null code

--- src/SaveIT.Web/Controllers/HomeController.cs
+++ src/SaveIT.Web/Controllers/HomeController.cs
@@ -23,7 +23,13 @@ public class HomeController : Controller
 
 	public IActionResult AuthorizeAccount(string code)
 	{
-		return Redirect("https://accounts.google.com/o/oauth2/v2/auth?" +
+		if (code is null)
+		{
+			// Handle Error
+			return View();
+		}
+
+		return RedirectPermanent("https://accounts.google.com/o/oauth2/v2/auth?" +
 			"scope=https://www.googleapis.com/auth/drive&" +
 			"access_type=offline&" +
 			"include_granted_scopes=true&" +

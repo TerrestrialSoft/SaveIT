3e3d7e1e4f39b480328e16dc19be579aed8e2f41
TheSecurity <jamiedesert01@gmail.com>
2022-10-13 09:24:14 +0200

    Introduce exception handling on web

--- src/SaveIT.Web/Program.cs
+++ src/SaveIT.Web/Program.cs
@@ -11,9 +11,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Home/Error");
+	app.UseExceptionHandler(errorHandlingPath: "/Error");
+	app.UseStatusCodePagesWithRedirects("/Error/{0}");
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
+} else
+{
+	app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
@@ -25,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
 	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+	pattern: "{controller=Auth}/{action=Index}/{id?}");
 
 app.Run();

81972bb45fce524c725f15210d759dc720ce1cbf
TheSecurity <jamiedesert01@gmail.com>
2022-10-13 08:22:00 +0200

    Add token storage

---
+++ src/SaveIT.Core/Dtos/OAuthTokenDto.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace SaveIT.Core.Dtos;
+public class OAuthTokenDto
+{
+	public string AcccessToken { get; set; } = null!;
+	public long ExpiresIn { get; set; }
+	public string RefreshToken { get; set; } = null!;
+	public string Scope { get; set; } = null!;
+	public string TokenType { get; set; } = null!;
+}
---
+++ src/SaveIT.Core/Services/ITokenService.cs
@@ -0,0 +1,11 @@
+﻿using SaveIT.Core.Dtos;
+
+namespace SaveIT.Core.Services;
+public interface ITokenService
+{
+	public Task<OAuthTokenDto?> GetTokenAsync(long profileId);
+	public Task CreateTokenAsync(long profileId, OAuthTokenDto token);
+	public Task UpdateTokenAsync(long profileId, OAuthTokenDto token);
+	public Task<bool> TokenExists(long profileId);
+	public string GetSecret();
+}

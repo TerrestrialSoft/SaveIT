c1d9b5008d652f08941e6a2851ddce0cd1865890
TheSecurity <jamiedesert01@gmail.com>
2024-03-31 17:10:30 +0200

    Add Local folder picker

---
+++ src/SaveIt.App/SaveIt.App.UI/Models/DirectoryModel.cs
@@ -0,0 +1,7 @@
+﻿namespace SaveIt.App.UI.Models;
+public class DirectoryModel
+{
+    public required string Name { get; set; } = default!;
+    public required string Path { get; set; } = default!;
+    public bool IsSelected { get; set; }
+}

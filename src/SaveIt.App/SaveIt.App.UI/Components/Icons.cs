b7be6153c88b3ff450ff6dd751aa671b1a76a79f
TheSecurity <jamiedesert01@gmail.com>
2024-04-21 10:54:43 +0200

    Add Icons provider

---
+++ src/SaveIt.App/SaveIt.App.UI/Components/Icons.cs
@@ -0,0 +1,8 @@
+﻿namespace SaveIt.App.UI.Components;
+public static class Icons
+{
+    public const string PenSolid = "fa-solid fa-pen";
+    public const string SettingsSolid = "fa-solid fa-gear";
+    public const string TrashSolid = "fa-solid fa-trash";
+    public const string ShareSolid = "fa-solid fa-share";
+}

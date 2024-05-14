efbc2541a3afd7c0e0798706bce20d63b65b2651
TheSecurity <jamiedesert01@gmail.com>
2024-05-14 12:36:52 +0200

    Add workaround for WinUI3 not properly drawing select options when moving window

--- src/SaveIt.App/SaveIt.App.UI/MainPage.xaml.cs
+++ src/SaveIt.App/SaveIt.App.UI/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace SaveIt.App.UI;
+﻿using Microsoft.Maui.Platform;
+
+namespace SaveIt.App.UI;
 
 public partial class MainPage : ContentPage
 {
@@ -6,4 +8,41 @@ public partial class MainPage : ContentPage
     {
         InitializeComponent();
     }
+
+#if WINDOWS
+    bool _foundWindow;
+
+    protected override void OnHandlerChanged()
+    {
+        base.OnHandlerChanged();
+
+        if (!_foundWindow)
+        {
+            var window = GetParentWindow();
+
+            if ((window?.Handler?.PlatformView as MauiWinUIWindow)?.GetAppWindow() is var appWindow)
+            {
+                appWindow.Changed += AppWindow_Changed;
+                _foundWindow = true;
+            }
+        }
+    }
+
+    private void AppWindow_Changed(Microsoft.UI.Windowing.AppWindow sender, Microsoft.UI.Windowing.AppWindowChangedEventArgs args)
+    {
+        if (args.DidPositionChange)
+        {
+            var width = Window.Width;
+
+            Window.Width = width + 1;
+            Window.Width = width;
+        }
+        else if (args.DidSizeChange)
+        {
+            WidthRequest = sender.Size.Width;
+            HeightRequest = sender.Size.Height;
+        }
+
+    }
+#endif
 }
